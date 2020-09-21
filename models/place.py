560bec4182d16e4bb8dadae0f21754614fed5275
David Szemenkar <dszemenkar@icloud.com>
2020-09-21 16:14:21 +0200

    Added image model and extended the place model.

--- models/place.py
+++ models/place.py
@@ -6,17 +6,29 @@ class PlaceModel(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80))
 	desc_sv = db.Column(db.String(3000))
+	desc_en = db.Column(db.String(3000))
+	desc_de = db.Column(db.String(3000))
+	lat = db.Column(db.Float(precision=6))
+	lon = db.Column(db.Float(precision=6))
+
+	images = db.relationship('ImageModel', lazy='dynamic')
 
 	location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
 	location = db.relationship('LocationModel')
 
-	def __init__(self, name, desc_sv, location_id):
+
+	def __init__(self, name, desc_sv, desc_en, desc_de, lat, lon, location_id):
 		self.name = name
 		self.desc_sv = desc_sv
+		self.desc_en = desc_en
+		self.desc_de = desc_de
+		self.lat = lat
+		self.lon = lon
 		self.location_id = location_id
 
 	def json(self):
-		return {'name': self.name, 'desc_sv': self.desc_sv}
+		return {'name': self.name, 'desc_sv': self.desc_sv, 'desc_en': self.desc_en, 'desc_de': self.desc_de, 
+		'lat': self.lat, 'lon': self.lon}
 
 	@classmethod
 	def find_by_name(cls, name):

7c1bf32be5e863bf62256d66c15b4c6a8210cafe
David Szemenkar <dszemenkar@icloud.com>
2020-09-23 15:08:31 +0200

    More fixes.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from security import authenticate, identify
 from resources.user import UserRegister
 from resources.place import Place, PlaceList
 from resources.location import Location, LocationList
+from resources.images import Image
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') 
@@ -22,7 +23,7 @@ api.add_resource(Place, '/places/<string:name>')
 api.add_resource(PlaceList, '/places')
 api.add_resource(LocationList, '/locations')
 api.add_resource(UserRegister, '/register')
-api.add_resource(ImageModel, '/images')
+api.add_resource(Image, '/images')
 
 if __name__ == '__main__':
 	db.init_app(app)

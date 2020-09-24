2cb47fc0600f5e69fe740105b4a5b53ca181c022
David Szemenkar <dszemenkar@icloud.com>
2020-09-24 19:54:18 +0200

    Added images to place return.

--- models/place.py
+++ models/place.py
@@ -27,7 +27,7 @@ class PlaceModel(db.Model):
 		self.location_id = location_id
 
 	def json(self):
-		return {'id': self:id, 'name': self.name, 'desc_sv': self.desc_sv, 'desc_en': self.desc_en, 'desc_de': self.desc_de, 'lat': self.lat, 'lon': self.lon}
+		return {'name': self.name, 'desc_sv': self.desc_sv, 'desc_en': self.desc_en, 'desc_de': self.desc_de, 'lat': self.lat, 'lon': self.lon, 'images': [images.json() for image in self.images.all()]}
 
 	@classmethod
 	def find_by_name(cls, name):
